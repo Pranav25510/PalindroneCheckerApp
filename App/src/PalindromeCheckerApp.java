0eb93ad3dc2d71dd717d496a3f358f891605c7a5
Pranav25510 <Pranav20000000@gmail.com>
2026-03-06 15:16:33 +0530

    [Pranav25510] Added [UC2-HardCCodePalindrome]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Palindrome Checker App Management System");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+
+        scanner.close();
     }
 }
